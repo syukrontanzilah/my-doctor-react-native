a731c94fa9382ab6f38ba29ea7510d4f2d05eb0f
syukrontanzilah <syukrontanzilah@gmail.com>
2020-06-06 19:01:54 +0700

    setup firebase

---
+++ src/config/Fire/index.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+
+firebase.initializeApp({
+    apiKey: "AIzaSyA1ivoVApTdozi1bs8_7bAoXJWEj0la748",
+    authDomain: "my-doctor-2020-e356b.firebaseapp.com",
+    databaseURL: "https://my-doctor-2020-e356b.firebaseio.com",
+    projectId: "my-doctor-2020-e356b",
+    storageBucket: "my-doctor-2020-e356b.appspot.com",
+    messagingSenderId: "508916658608",
+    appId: "1:508916658608:web:a0d6792f644d87108fa675"
+});
+
+const Fire = firebase;
+
+export default Fire;
