7799c3b699df2a0a75ecc95c0c170480b9560658
syukrontanzilah <syukrontanzilah@gmail.com>
2020-06-06 19:43:07 +0700

    integrasi register ke firebase

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import { Header, Input, Button, Gap } from '../../component'
 import { colors, useForm } from '../../utils'
+import { Fire } from '../../config'
 
-const Register = ({navigation}) => {
+const Register = ({ navigation }) => {
     // const [fullName, setFullName] = useState('');
     // const [profesi, setProfesi] = useState('');
     // const [email, setEmail] = useState('');
@@ -17,46 +18,55 @@ const Register = ({navigation}) => {
     })
 
     const onContinue = () => {
-        console.log(form)
-       // navigation.navigate('UploadFoto')
+        console.log(form);
+        Fire.auth()
+            .createUserWithEmailAndPassword(form.email, form.password)
+            .then(success => {
+                console.log('register success: ', success)
+            })
+            .catch(error => {
+                const errorMessage = error.message;
+                console.log('error register: ', errorMessage)
+            });
+        // navigation.navigate('UploadFoto')
     }
 
     return (
-        <View style ={styles.page}>
-            <Header onPress={()=> navigation.goBack()} title='Regitrasi'/>
+        <View style={styles.page}>
+            <Header onPress={() => navigation.goBack()} title='Regitrasi' />
 
-            <ScrollView style ={styles.content}>
-                <Input 
-                label='Full Name'
-                value={form.fullName} 
-                 onChangeText={value => setForm('fullName',value)}
+            <ScrollView style={styles.content}>
+                <Input
+                    label='Full Name'
+                    value={form.fullName}
+                    onChangeText={value => setForm('fullName', value)}
                 />
-                <Gap height={24}/>
+                <Gap height={24} />
 
-                <Input 
-                label='Pekerjaan'
-                value={form.profesi} 
-                 onChangeText={value=> setForm('profesi', value)}
+                <Input
+                    label='Pekerjaan'
+                    value={form.profesi}
+                    onChangeText={value => setForm('profesi', value)}
                 />
-                <Gap height={24}/>
+                <Gap height={24} />
 
-                <Input 
-                label='Email Address'
-                value={form.email} 
-                   onChangeText={(value)=> setForm('email',value)}
+                <Input
+                    label='Email Address'
+                    value={form.email}
+                    onChangeText={(value) => setForm('email', value)}
                 />
-                <Gap height={24}/>
+                <Gap height={24} />
 
-                <Input 
-                label='Password'
-                value={form.password} 
-                secureTextEntry
-                 onChangeText={(value)=> setForm('password',value)}
+                <Input
+                    label='Password'
+                    value={form.password}
+                    secureTextEntry
+                    onChangeText={(value) => setForm('password', value)}
                 />
-                <Gap height={28}/>
+                <Gap height={28} />
 
                 <Button title='Continue' onPress={onContinue} />
-                <Gap height={24}/>
+                <Gap height={24} />
 
 
             </ScrollView>
@@ -69,11 +79,11 @@ export default Register
 
 const styles = StyleSheet.create({
     page: {
-        backgroundColor:colors.white,
-        flex:1
+        backgroundColor: colors.white,
+        flex: 1
     },
     content: {
-        padding:40, 
-        paddingTop:0
+        padding: 40,
+        paddingTop: 0
     }
 })
