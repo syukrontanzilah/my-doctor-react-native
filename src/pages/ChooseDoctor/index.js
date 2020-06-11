8ba544f2757a863d642ea107c3c843b883f654db
syukrontanzilah <syukrontanzilah@gmail.com>
2020-06-11 09:28:49 +0700

    data dinamis klik choose dokter pindah ke dokter profile

--- src/pages/ChooseDoctor/index.js
+++ src/pages/ChooseDoctor/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Header, List } from '../../component'
-import { Doctor1, Doctor2, Doctor3, Doctor4, DoctorWanita1, DoctorPria1, DoctorWanita2, IconNext, DoctorPria2, DoctorPria3, DoctorWanita5, DoctorWanita6, DoctorWanita3, DoctorWanita7 } from '../../asset'
 import { colors } from '../../utils'
 import { Fire } from '../../config'
 
@@ -44,18 +43,13 @@ const ChooseDoctor = ({ navigation, route }) => {
                 listDoctor.map(doctor => {
                     return <List
                         key={doctor.id}
-                        onPress={() => navigation.navigate('Chatting')}
+                        onPress={() => navigation.navigate('DoctorProfile', doctor)}
                         profil={{ uri: doctor.data.photo }}
                         name={doctor.data.fullName}
                         chat={doctor.data.gender} type='next' />
 
                 })
             }
-
-
-            {/* <List profil={DoctorWanita2} name='Maissy Pramasella' chat='Wanita' type='next' />
-            <List profil={DoctorPria1} name='dr. Mike Andi Nugroho' chat='Pria' type='next' />
-            <List profil={DoctorPria3} name='dr. Azmi Furqon' chat='Pria' type='next' /> */}
 
         </View>
     )
